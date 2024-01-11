0e90f89abe8a418afa8b53079ffd6fcdf5e4ade6
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-01-11 11:37:13 +0200

    icons

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './styles/responsive.css';
 import Header from './components/layout/Header';
 import ToursSection from './components/sections/ToursSection';
 import ProjectsSection from './components/sections/ProjectsSection';
+import ServicesSection from './components/sections/ServicesSection';
 
 // const title = <h3 className='card__title'>New York</h3>;
 
@@ -10,8 +11,9 @@ function App() {
   return (
     <div className='App'>
       <Header />
-      <ToursSection />
-      <ProjectsSection />
+      {/* <ToursSection /> */}
+      {/* <ProjectsSection /> */}
+      <ServicesSection />
     </div>
   );
 }
---
+++ src/components/sections/ServicesSection.jsx
@@ -0,0 +1,35 @@
+import Grid from '../ui/Grid';
+import SectionTitle from '../ui/SectionTitle';
+
+function ServiceCard({ title, icon }) {
+  return (
+    <div className='service-card'>
+      <i className={`service-card__icon lnr ${icon}`}></i>
+      <h3 className='service-card__title'>{title}</h3>
+      <p className='service-card__text'>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+    </div>
+  );
+}
+
+function ServicesSection() {
+  return (
+    <section className='services'>
+      <div className='container'>
+        <SectionTitle
+          title='Our Services'
+          subtTitle='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+        />
+        <Grid className='services__content'>
+          <ServiceCard title='Web Design' icon='lnr-pie-chart' />
+          <ServiceCard title='Web Design' icon='lnr-laptop' />
+          <ServiceCard title='Web Design' icon='lnr-laptop' />
+        </Grid>
+      </div>
+    </section>
+  );
+}
+
+export default ServicesSection;
