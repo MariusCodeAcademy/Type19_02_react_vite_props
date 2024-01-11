752129080977c9946bb6d8dbd1cbedd525f64810
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-01-11 11:00:54 +0200

    class list

--- src/components/sections/ProjectsSection.jsx
+++ src/components/sections/ProjectsSection.jsx
@@ -4,15 +4,30 @@ function MyButton({ children }) {
   return <button>{children}</button>;
 }
 
-function Grid({ children }) {
-  return <div className='grid'>{children}</div>;
+function Grid({ children, colsNr }) {
+  // clase turetu buti 'grid' jei nepaduotas colsNr
+
+  // turetu buti 'grid grid-${colsNr}' jei paduotas colsNr
+
+  let resultClass = 'grid ';
+
+  if (colsNr) {
+    resultClass += `grid-${colsNr}`;
+  }
+
+  console.log('resultClass ===', resultClass);
+
+  return <div className={resultClass}>{children}</div>;
+  // return (
+  //   <div className={`grid ${colsNr ? `grid-${colsNr}` : ''}`}>{children}</div>
+  // );
 }
 
 export default function ProjectsSection() {
   return (
     <div className='container'>
       <SectionTitle title='Projects' />
-      <Grid cols='3'>
+      <Grid colsNr={0}>
         <MyButton>This is btn1</MyButton>
         <MyButton>This is btn2</MyButton>
         <MyButton>This is text</MyButton>
